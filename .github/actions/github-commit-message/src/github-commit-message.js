e0483ff7880524a96fd81609e817331ad663ce4c
Andrey Borha <diajelovic@gmail.com>
2020-09-07 15:43:01 +0300

    add telegram bot message action

--- .github/actions/github-commit-message/src/github-commit-message.js
+++ .github/actions/github-commit-message/src/github-commit-message.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  core.setOutput("last_commit_message", github.context.payload.head_commit.message);
+  core.setOutput('last_commit_message', github.context.payload.head_commit.message);
 } catch (error) {
   core.setFailed(error.message);
 }
