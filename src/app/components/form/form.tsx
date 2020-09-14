0af96025aac5ffa574b58ddddb85543a181e64d3
Andrey Borha <diajelovic@gmail.com>
2020-09-14 18:58:12 +0300

    add recovery

--- src/app/components/form/form.tsx
+++ src/app/components/form/form.tsx
@@ -7,16 +7,26 @@ type Props = React.PropsWithChildren<{
   title: string;
   onSubmit: (e: React.FormEvent) => void;
   buttons?: React.ReactElement;
-  errors: string[];
+  errors?: string[];
+  messages?: string[];
 }>;
 
 export const Form = (props: Props) => {
+  const { errors = [], messages = [] } = props;
+
   return (
     <form className={styles.form} onSubmit={props.onSubmit}>
       <h1>{props.title}</h1>
-      {!!props.errors.length && (
+      {!!messages.length && (
+        <ul className={styles.messagesWrapper}>
+          {messages.map((message, index) => {
+            return <li key={index}>{message}</li>;
+          })}
+        </ul>
+      )}
+      {!!errors.length && (
         <ul className={cn(styles.messagesWrapper, styles.errors)}>
-          {props.errors.map((error, index) => {
+          {errors.map((error, index) => {
             return <li key={index}>{error}</li>;
           })}
         </ul>
